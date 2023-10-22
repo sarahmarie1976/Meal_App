34f164de99ae1b235d999b37fca38b6ab36cfbc5
Bryan Jones <bryanjones@Bryans-MBP.attlocal.net>
2023-10-22 18:29:47 -0500

    :construction: created new files and added dummy data as well as categoriesScreen

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,19 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function CategoryGridTile({title, color}) {
+    return(
+        <View >
+            <Pressable >
+              <View >
+                <Text >
+                    {title}
+                </Text>
+              </View>
+            </Pressable>
+        </View>
+    )
+}
+
+export default CategoryGridTile
+
+const styles = StyleSheet.create({})
